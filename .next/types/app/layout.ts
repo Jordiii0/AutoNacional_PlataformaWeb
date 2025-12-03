ba86a8e72f3b7de6716f3d85392ec7596f44ce65
Matias Pardo <matias.pardo13@inacapmail.cl>
2025-12-03 13:41:11 -0300

    ..

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\ignja\Documents\semestre 8\Proyecto de Titulo\AutoNacional\app\layout.tsx
+// File: C:\Users\Matias Pardo\AutoNacional_PlataformaWeb\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
